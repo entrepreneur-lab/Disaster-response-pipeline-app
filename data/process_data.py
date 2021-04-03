4b077d16f295bfc0dfa9ec11eff9b41c522a8f00
martin <mkenny5@tcd.ie>
2021-04-03 21:50:47 +0100

    Completed ETL pipeline in process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,94 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    Load multiple data sources from csv files
+    and combine into a single dataframe
 
+    Parameters
+    ----------
+    messages_filepath : string
+        Path to the csv file containing categories data
+    categories_filepath : string
+        Path to the csv file containing categories data 
+
+    Returns
+    -------
+    df : Pandas DataFrame
+        Combined data from messages and categories csv files
+
+    """
+    
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    
+    df = pd.merge(messages, categories, on="id")
+    return df
 
 def clean_data(df):
-    pass
+    """
+    Cleaned the provided dataframe by creating columns
+    for each category and removing duplicate rows
+
+    Parameters
+    ----------
+    df : Pandas DataFrame
+        Raw, unformatted data
+
+    Returns
+    -------
+    df : Pandas DataFrame
+        Cleaned data to be saved in a database
+
+    """
+    
+    # create a dataframe of the 36 individual category columns
+    categories = df["categories"].str.split(';', expand=True)
+    
+    # get categories as column names 
+    row = categories.iloc[0,:]
+    category_colnames = [r.split('-')[0] for r in row]
+    categories.columns = category_colnames
+    
+    # create columns for each category
+    # columns are of type integer and are either 0 or 1
+    for column in categories:
+        categories[column] = categories[column].str[-1]
+        categories[column] = categories[column].astype(int)
+        
+    # drop the original categories column
+    # and combine with the formatted categories data
+    df = df.drop(columns=["categories"])
+    df = pd.concat([df, categories], axis=1)
+    
+    # remove duplicate rows
+    df = df.drop_duplicates()
+    
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """
+    Save the dataframe to a database
+
+    Parameters
+    ----------
+    df : Pandas DataFrame
+        Clean data to be saved in the database
+    database_filename : string
+        filename for the database
+
+    Returns
+    -------
+    None.
+
+    """
+    
+    engine = create_engine(f"sqlite:///{database_filename}.db")
+    df.to_sql(database_filename, engine, index=False)
 
 
 def main():
